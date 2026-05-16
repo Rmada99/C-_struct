8dfff0b3eeaa5a53966c87401b21c43f9281fe2e
XPS\ktc <ktc25a31a0057@edu.kyoto-tech.ac.jp>
2026-05-16 16:16:25 +0900

    課題２

---
+++ kadai2/kadai2/kadai2.cpp
@@ -0,0 +1,47 @@
+﻿
+#include <iostream>
+
+int maxscore = 0;
+float ave;
+
+struct StudentData
+{
+    char name[10];
+    int score;
+};
+
+StudentData no1student;
+
+void Point(StudentData studentdata) {
+
+    if (studentdata.score > maxscore) {
+        no1student = studentdata;
+        maxscore = studentdata.score;
+    }
+    
+    ave += studentdata.score;
+}
+
+int main()
+{
+    StudentData student1{ "player",20 };
+    Point(student1);
+
+    StudentData student2{ "enemy",60 };
+    Point(student2);
+
+    StudentData student3{ "monky",99 };
+    Point(student3);
+
+    StudentData student4{ "lion",2 };
+    Point(student4);
+
+    StudentData student5{ "slime",2000 };
+    Point(student5);
+
+    ave /= 5;
+
+    printf("平均点:%f\n",ave);
+    printf("最高点:%d\n", maxscore);
+    printf("最高得点者:%s\n", no1student.name);
+}
