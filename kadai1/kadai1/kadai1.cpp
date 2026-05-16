39e9c2a8066dbb507768cad43a4b09b6327c38c2
XPS\ktc <ktc25a31a0057@edu.kyoto-tech.ac.jp>
2026-05-16 15:48:10 +0900

    課題１

---
+++ kadai1/kadai1/kadai1.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+# include<string>
+using namespace std;
+
+struct Character
+{
+	char const name;
+	int hp;
+	int attack;
+	int defense;
+};
+
+void Data(Character character) {
+	
+	printf("名前:%c\n", character.name);
+	printf("HP:%d\n", character.hp);
+	printf("攻撃力:%d\n", character.attack);
+	printf("防御力:%d\n", character.defense);
+	printf("\n");
+
+}
+
+int main()
+{
+	Character chara1 = { 'name',100,20,30 };
+
+	Data(chara1);
+
+	Character chara2 = { 'enem',20,3,400 };
+
+	Data(chara2);
+
+}
